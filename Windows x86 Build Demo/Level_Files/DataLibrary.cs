14287195bad6debd664f117dbcbe64cf4ae51d63
Tanzim Khan <36486623+khan498@users.noreply.github.com>
2018-03-27 14:35:18 -0400

    Added a demo build

---
+++ Windows x86 Build Demo/Level_Files/DataLibrary.cs
@@ -0,0 +1,34 @@
+﻿namespace SuperMarioBrothers.Level_Files
+{
+    public class DataLibrary
+    {
+        private static DataLibrary instance = new DataLibrary();
+
+        private DataLibrary()
+        {
+        }
+
+        public static DataLibrary Instance
+        {
+            get { return instance; }
+        }
+        
+        public const int DEFAULT_SIZE = 32;
+
+        public const int BLOCK_SCALE = 1;
+        public const int ENEMY_SCALE = 1;
+        public const int PLAYER_SCALE = 1;
+        public const int ITEM_SCALE = 1;
+        public const int PIPE_SCALE = 1;
+        public const int CASTLE_SCALE = 1;
+
+        public const int BRICK_BLOCK_OFFSET = 12;
+        public const int COIN_OFFSET = 6;
+
+        public const int STAR_MARIO_TIMER = 500;
+        public const int PLAYER_MOVEMENT_SPEED = 3;
+        public const int DEFAULT_GRAVITY = 1;
+        public const int DEFAULT_LIVES = 3;
+        public const int ARENA_LIVES = 4;
+    }
+}
